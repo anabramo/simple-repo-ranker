fefff42f23330c46427b4ca03aff403d51130b91
Andrey Abramov <89972043+anabramo@users.noreply.github.com>
2024-04-24 13:10:54 +0100

    Updates

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python /main.py --openai-api-key "$1" --github-token "$2" --openai-engine "$5" --openai-temperature "$6" --openai-max-tokens "$7"
+python /main.py --openai-api-key "$1" --github-token "$2" --openai-engine "$3" --openai-temperature "$4" --openai-max-tokens "$5"
